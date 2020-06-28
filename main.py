b2086255b7222666e45fb101778ee9bcf67471fc
herbetelem <31862899+herbetelem@users.noreply.github.com>
2020-06-28 17:45:05 +0200

    add check win/loose

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import livraison as livraison
 import checkForTheWin as checkForTheWin
 import loose as loose
 import win as win
+import testson as son
 
 
 print("Bonjour, blablabla jeux")
@@ -46,76 +47,81 @@ mapPrintedEnergy = printMapEnergy.printMapEnergy(nbRecharge, mapEnergy)
 mapPrintedMine = printMapMine.printMapMine(nbSpot, mapMine)
 
 
-
 print(printMap.printMap(0, mapNormal))
 print(mapPrintedEnergy)
 print(mapPrintedMine)
 print("Il vous reste " + str(printEnergy.printEnergy(energy, 0, coutRun)) + " energie")
 
 while statut == "ok":
-    if energy <= 0:
-        statut = "ko"
-    
-    if positionTrain == 0 and tour > 0:
-        if checkForTheWin.checkForTheWin(mapPrintedMine, longVoie):
-            statut = "win"
-    else:
-        action = str(input("Que voulez vous faire ? rouler ou ramasser : "))
-        while action not in actionPossible:
-            action = str(input("rouler ou ramasser : "))
-        if action == "rouler":
-            deplacement = int(input("quel distance voulez vous parcourir (positif pour aller en avant et negatif pour l'arriere) : "))
-            while (positionTrain + deplacement) < 0 or (positionTrain + deplacement) > longVoie:
-                deplacement = int(input("veuillez ne pas sortir des voies "))
-            positionTrain = positionTrain + deplacement
-            energy = printEnergy.printEnergy(energy, abs(deplacement), coutRun)
-            
-        elif action == "ramasser":
-            if mapPrintedMine[positionTrain] == "-" and mapPrintedEnergy[positionTrain] == "-":
-                print("Il n'y a rien a ramsser ici")
-            elif checkInt.checkInt(mapPrintedMine[positionTrain]) and mapPrintedEnergy[positionTrain] == "z":
-                print("deux option s'offre avous, ramasser de l'energie ou des chargement")
-                choixRammasser = str(input("que souhaitez vous prendre: energie ou chargement : "))
-                choixPossible = ["energie", "chargement"]
-                while choixRammasser not in choixPossible:
-                    choixRammasser = str(input("energie ou chargement sont vos seuls option : "))
-                if choixRammasser == "energie":
-                    action = ramasserEnergy.ramasserEnergy(mapEnergy, energy, regenEnergy, positionTrain, coutCharge)
-                    energy = action[1]
-                    mapPrintedEnergy = action[0]
-                else:
-                    action = ramasserMine.ramasserMine(mapPrintedMine, energy, cargaison, positionTrain, coutCharge, maxCargaison)
-                    energy = action[1]
-                    mapPrintedMine = action[0]
-                    cargaison = action[2]
-            elif checkInt.checkInt(mapPrintedMine[positionTrain]):
-                action = ramasserMine.ramasserMine(mapPrintedMine, energy, cargaison, positionTrain, coutCharge, maxCargaison)
+    action = str(input("Que voulez vous faire ? rouler ou ramasser : "))
+    while action not in actionPossible:
+        action = str(input("rouler ou ramasser : "))
+    if action == "rouler":
+        deplacement = int(input(
+            "quel distance voulez vous parcourir (positif pour aller en avant et negatif pour l'arriere) : "))
+        while (positionTrain + deplacement) < 0 or (positionTrain + deplacement) > longVoie:
+            deplacement = int(input("veuillez ne pas sortir des voies "))
+        positionTrain = positionTrain + deplacement
+        energy = printEnergy.printEnergy(energy, abs(deplacement), coutRun)
+
+    elif action == "ramasser":
+        if mapPrintedMine[positionTrain] == "-" and mapPrintedEnergy[positionTrain] == "-":
+            print("Il n'y a rien a ramsser ici")
+        elif checkInt.checkInt(mapPrintedMine[positionTrain]) and mapPrintedEnergy[positionTrain] == "z":
+            print("deux option s'offre avous, ramasser de l'energie ou des chargement")
+            choixRammasser = str(
+                input("que souhaitez vous prendre: energie ou chargement : "))
+            choixPossible = ["energie", "chargement"]
+            while choixRammasser not in choixPossible:
+                choixRammasser = str(
+                    input("energie ou chargement sont vos seuls option : "))
+            if choixRammasser == "energie":
+                action = ramasserEnergy.ramasserEnergy(
+                    mapEnergy, energy, regenEnergy, positionTrain, coutCharge)
+                energy = action[1]
+                mapPrintedEnergy = action[0]
+            else:
+                action = ramasserMine.ramasserMine(
+                    mapPrintedMine, energy, cargaison, positionTrain, coutCharge, maxCargaison)
                 energy = action[1]
                 mapPrintedMine = action[0]
                 cargaison = action[2]
-            else:
-                action = ramasserEnergy.ramasserEnergy(mapEnergy, energy, regenEnergy, positionTrain, coutCharge)
-                energy = action[1]
-                mapPrintedEnergy = action[0]
+        elif checkInt.checkInt(mapPrintedMine[positionTrain]):
+            action = ramasserMine.ramasserMine(
+                mapPrintedMine, energy, cargaison, positionTrain, coutCharge, maxCargaison)
+            energy = action[1]
+            mapPrintedMine = action[0]
+            cargaison = action[2]
         else:
-            action = livraison.livraison(energy, positionTrain)
-            energy = action
-            cargaison = 0
-            positionTrain = 0
-        print(printMap.printMap(positionTrain, mapNormal))
-        print(mapPrintedEnergy)
-        print(mapPrintedMine)
-        print()
-        print("Il vous reste " + str(energy) + " energie")
-        print("Votre chagement est de " + str(cargaison) + "/" + str(maxCargaison) + ".")
-        if cargaison > 0:
-            print("Si vous souhaitez vider votre chargement saisissez 'livraison'.")
-        print()
-        tour += 1
+            action = ramasserEnergy.ramasserEnergy(
+                mapEnergy, energy, regenEnergy, positionTrain, coutCharge)
+            energy = action[1]
+            mapPrintedEnergy = action[0]
+    else:
+        action = livraison.livraison(energy, positionTrain)
+        energy = action
+        cargaison = 0
+        positionTrain = 0
+    print(printMap.printMap(positionTrain, mapNormal))
+    print(mapPrintedEnergy)
+    print(mapPrintedMine)
+    print()
+    print("Il vous reste " + str(energy) + " energie")
+    print("Votre chagement est de " + str(cargaison) + "/" + str(maxCargaison) + ".")
+    if cargaison > 0:
+        print("Si vous souhaitez vider votre chargement saisissez 'livraison'.")
+    print()
+    tour += 1
+    if positionTrain == 0 and tour > 0:
+        if checkForTheWin.checkForTheWin(mapPrintedMine, longVoie):
+            statut = "win"
+    elif energy <= 0:
+        statut = "ko"
 
 
 if statut == "ko":
     loose.loose(tour)
+    son.playMusicLost()
 
 elif statut == "win":
     win.win(tour)
--- testson.py
+++ testson.py
@@ -1,52 +1,26 @@
-# import winsound
-# frequency = 2500  # Set Frequency To 2500 Hertz
-# frequency2 = 5000  # Set Frequency To 2500 Hertz
-# duration = 1  # Set Duration To 1000 ms == 1 second
-# winsound.Beep(frequency2, 50)
-# winsound.Beep(frequency, 100)
-# winsound.Beep(frequency2, 50)
-# winsound.Beep(frequency, 100)
-# winsound.Beep(frequency2, 50)
-# winsound.Beep(frequency, 100)
-# winsound.Beep(frequency2, 50)
-# winsound.Beep(frequency, 100)
+import winsound
+
+def playMusicLost():
+	re = 146
+	mi = 164
+	fa = 174
+	sol = 196
+	la = 220
+	laB = 233
+	si = 246
+	siB = 255
+	do = 261
 
 
-import platform
-try:
-    import winsound
-    beep = winsound.Beep
-except ImportError:
-    def beep(f, d):
-        s = 8000
-        hp = int(s/f/2)
-        b = chr(255)*hp+chr(0)*hp
-        b *= int(d*f)
-        a = file('/dev/audio', 'wb')
-        a.write(b)
-        a.close()    
-
-c = [
-	(880, 700),
-	(587, 1000),
-	(698, 500),
-	(880, 500),
-	(587, 1000),
-	(698, 500),
-	(880, 250),
-	(1046, 250),
-	(988, 500),
-	(784, 500),
-	(699, 230),
-	(784, 250),
-	(880, 500),
-	(587, 500),
-	(523, 250),
-	(659, 250),
-	(587, 750)
-]
-
-s = c + c
-
-for f, d in s:
-	beep(f, d)
+	winsound.Beep(do, 800)
+	winsound.Beep(sol, 700)
+	winsound.Beep(mi, 750)
+	winsound.Beep(la, 350)
+	winsound.Beep(si, 400)
+	winsound.Beep(la, 300)
+	winsound.Beep(laB, 300)
+	winsound.Beep(siB, 300)
+	winsound.Beep(laB, 320)
+	winsound.Beep(mi, 180)
+	winsound.Beep(re, 280)
+	winsound.Beep(mi, 600)
