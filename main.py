eb4b6b98020c8c2b16c1949f84de3cb422ac82be
herbetelem <31862899+herbetelem@users.noreply.github.com>
2020-06-28 18:30:29 +0200

    FINALY END AN EN VERSION

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Import All Functions
 import printMap as printMap
 import printMapEnergy as printMapEnergy
 import printMapMine as printMapMine
@@ -16,10 +17,11 @@ import checkIsInt as checkIsInt
 print("Bonjour, blablabla jeux")
 print()
 
+# Set up variable, in auto mode or manual
 mode = str(input("mode auto, oui ou non ? "))
 if mode == "oui":
-    longVoie = 50
-    maxCargaison = 10
+    longWay = 50
+    MaxCargo = 10
     nbSpot = 20
     nbRecharge = 5
     energy = 200
@@ -27,8 +29,8 @@ if mode == "oui":
     coutRun = 1
     coutCharge = 2
 else:
-    longVoie = int(input("longueur de la voie : "))
-    maxCargaison = int(input("nombre de cargason max : "))
+    longWay = int(input("longueur de la voie : "))
+    MaxCargo = int(input("nombre de cargason max : "))
     nbSpot = int(input("nombre de spot : "))
     nbRecharge = int(input("nombre de recharge : "))
     energy = int(input("max energie : "))
@@ -36,11 +38,11 @@ else:
     coutRun = int(input("cout d'energie par deplacement : "))
     coutCharge = int(input("cout d'energie par chargement : "))
 
-tour = 0
-cargaison = 0
-mapNormal = ["="] * longVoie
-mapEnergy = ["-"] * longVoie
-mapMine = ["-"] * longVoie
+lap = 0
+cargo = 0
+mapNormal = ["="] * longWay
+mapEnergy = ["-"] * longWay
+mapMine = ["-"] * longWay
 positionTrain = 0
 statut = "ok"
 actionPossible = ["ramasser", "rouler", "livraison"]
@@ -48,51 +50,56 @@ mapPrintedEnergy = printMapEnergy.printMapEnergy(nbRecharge, mapEnergy)
 mapPrintedMine = printMapMine.printMapMine(nbSpot, mapMine)
 
 
+
+#print First map
 print(printMap.printMap(0, mapNormal))
 print(mapPrintedEnergy)
 print(mapPrintedMine)
 print("Il vous reste " + str(printEnergy.printEnergy(energy, 0, coutRun)) + " energie")
 
+
+
+# Core Function
 while statut == "ok":
     action = str(input("Que voulez vous faire ? rouler ou ramasser : "))
     while action not in actionPossible:
         action = str(input("rouler ou ramasser : "))
     if action == "rouler":
-        deplacement = int(input(
+        moove = int(input(
             "quel distance voulez vous parcourir (positif pour aller en avant et negatif pour l'arriere) : "))
-        while (positionTrain + deplacement) < 0 or (positionTrain + deplacement) > longVoie:
-            deplacement = int(input("veuillez ne pas sortir des voies "))
-        positionTrain = positionTrain + deplacement
-        energy = printEnergy.printEnergy(energy, abs(deplacement), coutRun)
+        while (positionTrain + moove) < 0 or (positionTrain + moove) > longWay:
+            moove = int(input("veuillez ne pas sortir des voies "))
+        positionTrain = positionTrain + moove
+        energy = printEnergy.printEnergy(energy, abs(moove), coutRun)
 
     elif action == "ramasser":
         if mapPrintedMine[positionTrain] == "-" and mapPrintedEnergy[positionTrain] == "-":
             print("Il n'y a rien a ramsser ici")
         elif checkInt.checkInt(mapPrintedMine[positionTrain]) and mapPrintedEnergy[positionTrain] == "z":
             print("deux option s'offre avous, ramasser de l'energie ou des chargement")
-            choixRammasser = str(
+            ChoicePickUp = str(
                 input("que souhaitez vous prendre: energie ou chargement : "))
             choixPossible = ["energie", "chargement"]
-            while choixRammasser not in choixPossible:
-                choixRammasser = str(
+            while ChoicePickUp not in choixPossible:
+                ChoicePickUp = str(
                     input("energie ou chargement sont vos seuls option : "))
-            if choixRammasser == "energie":
+            if ChoicePickUp == "energie":
                 action = ramasserEnergy.ramasserEnergy(
                     mapEnergy, energy, regenEnergy, positionTrain, coutCharge)
                 energy = action[1]
                 mapPrintedEnergy = action[0]
             else:
                 action = ramasserMine.ramasserMine(
-                    mapPrintedMine, energy, cargaison, positionTrain, coutCharge, maxCargaison)
+                    mapPrintedMine, energy, cargo, positionTrain, coutCharge, MaxCargo)
                 energy = action[1]
                 mapPrintedMine = action[0]
-                cargaison = action[2]
+                cargo = action[2]
         elif checkInt.checkInt(mapPrintedMine[positionTrain]):
             action = ramasserMine.ramasserMine(
-                mapPrintedMine, energy, cargaison, positionTrain, coutCharge, maxCargaison)
+                mapPrintedMine, energy, cargo, positionTrain, coutCharge, MaxCargo)
             energy = action[1]
             mapPrintedMine = action[0]
-            cargaison = action[2]
+            cargo = action[2]
         else:
             action = ramasserEnergy.ramasserEnergy(
                 mapEnergy, energy, regenEnergy, positionTrain, coutCharge)
@@ -101,29 +108,29 @@ while statut == "ok":
     else:
         action = livraison.livraison(energy, positionTrain)
         energy = action
-        cargaison = 0
+        cargo = 0
         positionTrain = 0
     print(printMap.printMap(positionTrain, mapNormal))
     print(mapPrintedEnergy)
     print(mapPrintedMine)
     print()
     print("Il vous reste " + str(energy) + " energie")
-    print("Votre chagement est de " + str(cargaison) + "/" + str(maxCargaison) + ".")
-    if cargaison > 0:
+    print("Votre chagement est de " + str(cargo) + "/" + str(MaxCargo) + ".")
+    if cargo > 0:
         print("Si vous souhaitez vider votre chargement saisissez 'livraison'.")
     print()
-    tour += 1
-    if positionTrain == 0 and tour > 0:
-        if checkForTheWin.checkForTheWin(mapPrintedMine, longVoie):
+    lap += 1
+    if positionTrain == 0 and lap > 0:
+        if checkForTheWin.checkForTheWin(mapPrintedMine, longWay):
             statut = "win"
     elif energy <= 0:
         statut = "ko"
 
 
 if statut == "ko":
-    loose.loose(tour)
+    loose.loose(lap)
     son.playMusicLost()
 
 elif statut == "win":
-    win.win(tour)
+    win.win(lap)
     son.playMusicWin()
